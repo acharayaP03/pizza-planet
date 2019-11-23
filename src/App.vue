54180f3a1c75eb079094f842d79014c76cfcd37e
acharyaP03 <acharyap03@gmail.com>
2019-11-23 13:49:57 +1100

    adding components for the admin section with static data

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <pp-header ></pp-header>
     </div>
     <div class="container">
-      <pp-menu></pp-menu>
+      <pp-admin></pp-admin>
     </div>
     <div class="container">
       <pp-footer></pp-footer>
@@ -17,6 +17,7 @@ import Header from './components/Header';
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Menu from "./components/Menu";
+import Admin from './components/Admin';
 export default {
     name: 'app', 
     data(){
@@ -28,7 +29,8 @@ export default {
         ppHeader: Header,
         ppFooter: Footer,
         ppHome: Home,
-        ppMenu: Menu
+        ppMenu: Menu,
+        ppAdmin: Admin
     }
 }
 </script>
